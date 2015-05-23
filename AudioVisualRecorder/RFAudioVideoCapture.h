3f1ec0127adcb7b5c7de158a43e5a379b869683a
Nicole Lehrer <nicole.lehrer@gmail.com>
2015-05-23 14:47:30 -0400

    added a new file for refactored code for capture vc, includes some notes from documentation

---
+++ AudioVisualRecorder/RFAudioVideoCapture.h
@@ -0,0 +1,13 @@
+//
+//  RFAudioVideoCapture.h
+//  AudioVisualRecorder
+//
+//  Created by Nicole Lehrer on 5/23/15.
+//  Copyright (c) 2015 Nicole Lehrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFAudioVideoCapture : UIViewController
+
+@end
---
+++ AudioVisualRecorder/RFAudioVideoCapture.m
@@ -0,0 +1,56 @@
+//
+//  RFAudioVideoCapture.m
+//  AudioVisualRecorder
+//
+//  Created by Nicole Lehrer on 5/23/15.
+//  Copyright (c) 2015 Nicole Lehrer. All rights reserved.
+//
+
+#import "RFAudioVideoCapture.h"
+
+@interface RFAudioVideoCapture ()
+
+@end
+
+@implementation RFAudioVideoCapture
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+//documentation
+//you need to specify a
+// 1 - device
+// 2 - input, to configure ports
+// 3 - output file type
+// 4 - session to corrdinate
+// optional 5 - preview layer (CALayer)
+
+// 6 - connection object manages connection between input port(s) and output 
+
+- (void)createCaptureSession{
+    
+}
+
+
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
